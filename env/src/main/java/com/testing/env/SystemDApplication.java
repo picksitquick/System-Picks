67b81fba1648b78884e39abdcfcd66487f114052
picksitquick <picksitquick@gmail.com>
2023-08-28 22:55:51 +0530

    intial project setup

---
+++ env/src/main/java/com/testing/env/SystemDApplication.java
@@ -0,0 +1,13 @@
+package com.testing.env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDApplication.class, args);
+	}
+
+}
---
+++ env/src/test/java/com/testing/env/SystemDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
